b661d6508f7da424e4533701284195b517d93280
Ray Kuo <ray.kuo@sap.com>
2025-10-26 14:42:10 -0700

    Initial import of VoiceBuy project

---
+++ VoiceBuyElderly/VoiceBuyElderly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceBuyElderly
+//
+//  Created by Kuo, Ray on 10/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
